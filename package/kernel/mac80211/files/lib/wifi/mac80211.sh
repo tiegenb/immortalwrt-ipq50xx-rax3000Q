236698559ef388478c1df8bb96d3d96584a6e8c8
tiegenb <334225021@qq.com>
2026-04-06 06:01:55 +0800

    Update mac80211.sh

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -167,25 +167,37 @@ detect_mac80211() {
 			dev_id="set wireless.radio${devidx}.macaddr=$(cat /sys/class/ieee80211/${dev}/macaddress)"
 		fi
 
+		# 根据频段设置不同的 SSID 和功率
+		if [ "$mode_band" = "2g" ]; then
+			ssid_name="铁哥中继器-2.4G"
+			txpower_val="18"
+			htmode_val="HE40"
+		else
+			ssid_name="铁哥中继器-5G"
+			txpower_val="30"
+			htmode_val="HE80"
+		fi
+
 		uci -q batch <<-EOF
 			set wireless.radio${devidx}=wifi-device
 			set wireless.radio${devidx}.type=mac80211
 			${dev_id}
 			set wireless.radio${devidx}.channel=${channel}
 			set wireless.radio${devidx}.band=${mode_band}
-			set wireless.radio${devidx}.htmode=$htmode
-			set wireless.radio${devidx}.country=CN
+			set wireless.radio${devidx}.htmode=${htmode_val}
+			set wireless.radio${devidx}.country=US
 			set wireless.radio${devidx}.disabled=0
+			set wireless.radio${devidx}.txpower=${txpower_val}
+			set wireless.radio${devidx}.cell_density=0
+			set wireless.radio${devidx}.mu_beamformer=1
 
 			set wireless.default_radio${devidx}=wifi-iface
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=ImmortalWrt
+			set wireless.default_radio${devidx}.ssid=${ssid_name}
 			set wireless.default_radio${devidx}.encryption=none
-EOF
-		uci -q commit wireless
-
+		EOF
 		devidx=$(($devidx + 1))
 	done
 }
